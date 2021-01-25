b9d48f786c38a3054cb298f844783cb3ce351c83
Kode <kodestar@gmail.com>
2021-01-25 11:44:05 +0000

    Updated service details

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -11,8 +11,12 @@
       </p>
       <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
       <h2>Crematorium</h2>
-      <p>The service is being held at woodlands crematorium</p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12:00pm</a>
+      <p>The service is being held at woodlands crematorium. To view it click the button below and enter the username and password supplied below.</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://www.obitus.com/index.php?page=login">Livestream - 12:00pm</a>
+      <ul class="fa-ul">
+        <ListItem>Username: <span class="highlight">Pita3729</span> <CopyButton text="Pita3729"></CopyButton></ListItem>
+        <ListItem>Password: <span class="highlight">871359</span> <CopyButton text="871359"></CopyButton></ListItem>
+      </ul>
     </div>
   </div>
 </template>
