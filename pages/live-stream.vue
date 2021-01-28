f1f3929ab08efaf3bc0e5cd71abac23b1f07c090
Kode <kodestar@gmail.com>
2021-01-28 14:37:28 +0000

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -7,16 +7,17 @@
       <h1 class="title">Service Livestream</h1>
       <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Connection Church Centre then at Woodlands Crematorium.</p>
       <h2>Church Service</h2>
-      <p>The church service is being performed at Connection Church Centre - Kingshurst, and being livestreamed on youtube. You can either click the button below or search for "lyn hunt memorial"
+      <p>The church service was performed at Connection Church Centre - Kingshurst, and livestreamed on youtube. It is still available to watch for those who missed it or want to see it again, by clicking the button below.
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="https://youtu.be/LSD6XzJjxWg">Livestream - 10:30am</a>
-      <h2>Crematorium</h2>
+      <a class="button" target="_blank" rel=”noopener” href="https://youtu.be/LSD6XzJjxWg">Watch now</a>
+      <p>Service actually starts at around 17 minutes</p>
+      <!--<h2>Crematorium</h2>
       <p>The service is being held at woodlands crematorium. To view it click the button below and enter the username and password supplied below.</p>
       <a class="button" target="_blank" rel=”noopener” href="https://www.obitus.com/index.php?page=login">Livestream - 12:00pm</a>
       <ul class="fa-ul">
         <ListItem>Username: <span class="highlight">Pita3729</span> <CopyButton text="Pita3729"></CopyButton></ListItem>
         <ListItem>Password: <span class="highlight">871359</span> <CopyButton text="871359"></CopyButton></ListItem>
-      </ul>
+      </ul>-->
     </div>
   </div>
 </template>
